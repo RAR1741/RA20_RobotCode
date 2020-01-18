43a5a4d7386167716cd5d074357d997c4caef5ac
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-01-18 14:12:50 -0500

    Update CAN ID's

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,7 +43,7 @@ public class Robot extends TimedRobot {
     System.out.print("Initializing shooter...");
     shooter = new Shooter(new CANSparkMax(2, MotorType.kBrushless));
     System.out.println("Initializing drivetrain...");
-    drive = new Drivetrain(2, 3, 4, 5, 6, 7);
+    drive = new Drivetrain(4, 5, 6, 7, 8, 9);
     System.out.println("done");
 
     System.out.print("Initializing driver interface...");
