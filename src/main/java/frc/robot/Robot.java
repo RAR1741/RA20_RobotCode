a1840dc202b9d4f072d24ac743f3aa4fafc6f8a7
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-01-20 18:59:47 -0500

    Initialize indexing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -73,7 +73,7 @@ public class Robot extends TimedRobot {
     System.out.println("done");
 
     System.out.print("Initializing intake...");
-    manipulation = new Manipulation(new Talon(11), new DoubleSolenoid(1, 2));
+    manipulation = new Manipulation(new Talon(11), new DoubleSolenoid(1, 2), new Talon(12), new Talon(13));
     System.out.println("done");
 
     System.out.print("Initializing driver interface...");
