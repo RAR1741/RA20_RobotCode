14ed0314c2c95df1172334b692cbe69f8701d02f
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-01-20 14:13:00 -0500

    Add documentation

--- src/main/java/frc/robot/Manipulation.java
+++ src/main/java/frc/robot/Manipulation.java
@@ -9,24 +9,42 @@ public class Manipulation{
     private Talon intakeWheel;
     private DoubleSolenoid intakePneumatics;
 
+    /**
+     * Constructor
+     * 
+     * @param intakeWheel The CAN id of the talon for the intake system
+     * @param intakePneumatics The intake solenoid
+     */
     Manipulation(Talon intakeWheel, DoubleSolenoid intakePneumatics) {
         this.intakeWheel = intakeWheel;
         this.intakePneumatics = intakePneumatics;
     }
 
+    /**
+     * Rotates the intake motor.
+     */
     public void intakeSpin(){
         //TODO: test if this power is right
         intakeWheel.set(1);
     }
 
+    /**
+     * Stops the rotation of the intake motor.
+     */
     public void intakeStop(){
         intakeWheel.set(0);
     }
 
+    /**
+     * Extends the intake system.
+     */
     public void intakeOut(){
         intakePneumatics.set(Value.kForward);
     }
 
+    /**
+     * Retracts the intake system.
+     */
     public void intakeIn(){
         intakePneumatics.set(Value.kReverse);
     }
