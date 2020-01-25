7d7b2cfcbea336d93c72b785a73cf2be82e97084
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-01-25 14:00:42 -0500

    Add aiming to x coordinate

---
+++ src/main/java/frc/robot/AutoAim.java
@@ -0,0 +1,49 @@
+package frc.robot;
+
+
+public class AutoAim {
+
+    private final double P;
+    
+    private double error;
+    private double motorPower;
+
+    public enum AutoAimState {
+        AIM_X,
+        AIM_ANGLE,
+        IDLE;
+    }
+
+    private AutoAimState state;
+    private Drivetrain drive;
+    private Limelight limelight;
+
+    public AutoAim(Drivetrain drive, Limelight limelight) {
+        state = AutoAimState.AIM_X;
+        this.drive = drive;
+        this.limelight = limelight;
+    }
+
+    public void run() {
+        switch(state) {
+
+            case AIM_X:
+                aimX(limelight.getTargetX());
+                break;
+
+            case AIM_ANGLE:
+                //aimAngle(limelight.getTargetVertical());
+                break;
+
+            case IDLE:
+                break;
+        }
+    }
+
+    private void aimX(double x) {
+        error = x * P;
+        motorPower = .5 * error;
+        drive.driveLeft(-motorPower);
+        drive.driveRight(motorPower);
+    }
+}
