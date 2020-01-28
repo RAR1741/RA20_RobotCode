9014fee637ac6606bf5079d2a5374ee39cc031b1
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-01-27 20:41:43 -0500

    Fix imports

--- src/main/java/frc/robot/PowercellDetection.java
+++ src/main/java/frc/robot/PowercellDetection.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
