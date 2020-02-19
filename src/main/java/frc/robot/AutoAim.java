d2f018b5ac4608d1bbf17cb2d13c9e9ae5e6628a
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-02-18 19:49:41 -0500

    Organize states into methods

--- src/main/java/frc/robot/AutoAim.java
+++ src/main/java/frc/robot/AutoAim.java
@@ -3,9 +3,6 @@ package frc.robot;
 
 public class AutoAim {
 
-    private double REVOLUTIONS_PER_DEGREE = 0; //TODO: Determine revolutions of motor required to turn angle motor a degree
-    private double ENCODERS_PER_REVOLUTION = 42;
-
     private double error;
     private double motorPower;
     private double degrees;
@@ -40,15 +37,17 @@ public class AutoAim {
         switch(state) {
 
             case AIM_X:
-                aimX(limelight.getTargetX());
-                if(Math.abs(limelight.getTargetX()) < 1)
+                AimX();
+                if (getWithinTolerance(0, limelight.getTargetX(), 1)) {
                     state = AutoAimState.AIM_ANGLE;
+                }
                 break;
 
             case AIM_ANGLE:
-                aimAngle(limelight.getTargetVertical());
-                if (Math.abs(degrees - shooter.getAngleInDegrees()) < 1)
+                AimAngle();
+                if (getWithinTolerance(degrees, shooter.getAngleInDegrees(), 1)) {
                     state = AutoAimState.IDLE;
+                }
                 break;
 
             case IDLE:
@@ -58,11 +57,9 @@ public class AutoAim {
 
     /**
      * Aims to robot to the X-coordinate of the power port.
-     * 
-     * @param x current X-coordinate of the power port.
      */
-    private void aimX(double x) {
-        error = x * 1;
+    private void AimX() {
+        error = limelight.getTargetX() * 1;
         motorPower = .5 * error;
         drive.driveLeft(motorPower);
         drive.driveRight(-motorPower);
@@ -70,11 +67,21 @@ public class AutoAim {
 
     /**
      * Aims the shooter angle to the appropriate angle, based on distance, to the power port.
-     * 
-     * @param h current height of the power port.
      */
-    private void aimAngle(double h) {
+    private void AimAngle() {
         degrees = 0; //TODO: Use a table from testing to create an equation to plug this into
         shooter.setAngle(degrees);
+    }
+
+    /**
+     * Gets if input is within tolerance of its goal.
+     * 
+     * @param goal desired value of input.
+     * @param current current value of input.
+     * @param tolerance tolerance of current input to desired input.
+     * @return true if within tolerance, false if not within tolerance.
+     */
+    private boolean getWithinTolerance(double goal, double current, double tolerance) {
+        return Math.abs(goal - current) < tolerance;
     }
 }
