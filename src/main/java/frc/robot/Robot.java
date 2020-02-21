64ecd37697bc3f5a958dacc3727c050c4b63c1e3
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2020-02-20 19:07:48 -0500

    Fix merge error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,8 +59,6 @@ public class Robot extends TimedRobot {
 
   double speed = 0;
 
-  double speed = 0;
-
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
