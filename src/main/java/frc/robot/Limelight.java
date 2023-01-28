81f464adc4b90ccf0ecbf9d8b0597a262c390bde
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-01-28 15:55:02 -0500

    collect all limelight info into a Map this has taken me A WEEK to get working

--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -3,6 +3,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -11,6 +13,8 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Limelight {
     NetworkTable limelight;
     LinkedHashMap<String, Object> limelightInfo;
+
+    ArrayList<Double> defaultDoubleArray = new ArrayList<Double>();
 
     /**
      * Constructor
@@ -25,8 +29,19 @@ public class Limelight {
      */
     public void update() {
         for (String key: limelight.getKeys()) {
-            String type = limelight.getEntry(key).getType().name();
-            SmartDashboard.putString(key, type);
+            String type = limelight.getEntry(key).getType().name().substring(1);
+
+            if (type.equals("String")) {
+                limelightInfo.put(key, limelight.getEntry(key).getString("No value"));
+            }
+            else if (type.equals("Double")) {
+                limelightInfo.put(key, limelight.getEntry(key).getDouble(Double.NaN));
+            }
+            else if (type.equals("DoubleArray")) {
+                limelightInfo.put(key, limelight.getEntry(key).getDoubleArray(new double[0]));
+            }
+
+            SmartDashboard.putString(key, (type.equals("String") || type.equals("Double")) ? limelightInfo.get(key).toString() : Arrays.toString((double[]) limelightInfo.get(key)));
         }
     }
 
